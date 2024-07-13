291d4bb2a3cbccdbbe45dd87a5427235e9aca923
Your Name <bartosz.kociecki@gmail.com>
2024-07-13 19:08:53 +0200

    first structures

---
+++ numpy1.py
@@ -0,0 +1,13 @@
+import numpy as np
+import os
+
+selector = "-----------------------"
+data = np.array([[1, -2, -4, 5], [4, 8, 3, 2]])
+print(data)
+print(selector)
+
+print(data*data)
+print(selector)
+
+print(data+10)
+print(selector)
