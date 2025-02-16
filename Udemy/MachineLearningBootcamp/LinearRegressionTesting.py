5f6342be4f58042964980766905a3b0302cf11fa
Your Name <bartosz.kociecki@gmail.com>
2025-02-16 18:50:20 +0100

    update

---
+++ Udemy/MachineLearningBootcamp/LinearRegressionTesting.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+#importujemy punkty
+df = pd.read_csv('cost_revenue_clean.csv', sep=';', decimal=',')
+
+#wyodrębniamy dane wejściowe (wszystkie wiersze, wszystkie kolumny oprócz ostatniej)
+X = df.values[:, :-1]
+
+#wyodrębniamy kolumnę wyjściową (wszystkie wiersz, ostatnia kolumna)
+Y = df.values[:, -1]
+
+#dopasowujemy linię do punktów
+dopasowanie = LinearRegression().fit(X, Y)
+
+m = dopasowanie.coef_.flatten()
+b = dopasowanie.intercept_.flatten()
+
+print("m = {0}".format(m))
+print("b = {0}".format(b))
+
+#pokazujemy na wykresie
+plt.plot(X, Y, 'o') #wykres punktowy
+plt.plot(X, m*X+b) #linia
+plt.show()
