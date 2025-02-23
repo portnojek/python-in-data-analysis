9285063e4bca81d9bcf37f217fe2dd67dafb0c80
Your Name <bartosz.kociecki@gmail.com>
2025-02-23 09:32:22 +0100

    update

--- DataCamp/DataPreProcessing/ConversionData.py
+++ DataCamp/DataPreProcessing/ConversionData.py
@@ -9,6 +9,8 @@ print(volunteer["production_budget_usd"].head())
 
 # Convert the hits column to type int
 volunteer["production_budget_usd"] = volunteer["production_budget_usd"].astype("int")
+volunteer["worldwide_gross_usd"] = volunteer["worldwide_gross_usd"].astype("int")
 
 # Look at the dtypes of the dataset
 print(volunteer.dtypes)
+print(volunteer)
